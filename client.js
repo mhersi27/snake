58aced741bb9e330f1d630c50a82a331add4a751
mhersi27 <mahamedabdullahi277@gmail.com>
2023-09-30 13:38:13 -0400

    added client.js

---
+++ client.js
@@ -0,0 +1,24 @@
+const {PORT, IP} = require('./constants');
+const net = require('net');
+
+const connect = () => {
+  const conn = net.createConnection({
+    port: PORT,
+    host: IP
+  });
+
+conn.setEncoding('utf8');
+
+conn.on('data', (message) =>{
+  console.log(message);
+});
+
+conn.on('connect', message => {
+  console.log('Now connected to the game server.');
+  conn.write("Name: MAH");
+});
+
+return conn;
+}
+
+module.exports = {connect};
